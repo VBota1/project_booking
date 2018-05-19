4f1c88b7a54b498437727dc48454f47661767ccc
Viorel Bota <botaviorel@gmail.com>
2018-05-19 17:12:29 +0300

    implemented CLI

--- project_booking_backend/src/lib.rs
+++ project_booking_backend/src/lib.rs
@@ -2,92 +2,95 @@ extern crate todo;
 use todo::ToDo;
 extern crate logger;
 use logger::*;
-use std::env;
 
-fn backend_main() {
+//TODO MEDIUM PRIO GUI (QT)
+//TODO MEDIUM PRIO import tasks from Jira
+//TODO MEDIUM PRIO export tasks to PTT
+//TODO LOW PRIO detect AFK and stop recoding
+//TODO LOW PRIO detect return on Keyboard and ask what task I am working on
 
-    match initiate_logging(None) {
-        Ok(message) => { trace(message); },
-        Err(message) => { error(message); return; },
-    };
+pub fn handle_command(mut args: std::env::Args, to_do: &mut ToDo) -> Result<String, String> {
 
-    let mut to_do = match ToDo::load(None) {
-        Ok(todo) => { trace(format!("Jobs loaded from database")); todo },
-        Err(error) => { warn(error.to_string()); ToDo::new() },
-    };
-
-    let args: Vec<String> = env::args().collect();
-    trace(format!("User application call: {}", args.join(" ")));
-    handle_command(env::args(), &mut to_do);
-
-    match to_do.save(None) {
-        Ok(_) => {},
-        Err(message) => { error(message); },
-    };
-
-    //TODO MEDIUM PRIO GUI (QT)
-
-    //TODO MEDIUM PRIO import tasks from Jira
-
-    //TODO MEDIUM PRIO export tasks to PTT
-
-    //TODO LOW PRIO detect AFK and stop recoding
-
-    //TODO LOW PRIO detect return on Keyboard and ask what task I am working on
-}
-
-fn handle_command(mut args: std::env::Args, to_do: &mut ToDo) {
     match args.nth(1) {
         Some(command) => {
             match command.as_str() {
                 "new" => {
                     trace(format!("New task request detected"));
-                    create_new_task_from_arguments(args, to_do);
+                    create_new_task_from_arguments(args, to_do)
                 },
                 "clockIn" => {
                     //TODO HIGH PRIO start to keep track of time (on task) (external thread)
+                    Err(format!("clockIn command not implemented"))
                 },
                 "clockOut" => {
                     //TODO HIGH PRIO stop to keep track of time (on task)
                     //TODO HIGH PRIO add recorded duration to task
+                    Err(format!("clockOut command not implemented"))
                 },
                 "report" => {
                     //TODO HIGH PRIO report time spent on one or all tasks
                     //TODO HIGH PRIO report time spent on one or all labels
+                    Err(format!("report command not implemented"))
                 },
                 "help" => {
                     //TODO HIGH PRIO return help information
+                    Err(format!("help command not implemented"))
                 },
                 _ => {
-                    error(format!("Unkown command \"{}\". {}", command, recommend_help()));
-                    return;
+                    let result = format!("Unkown command \"{}\". {}", command, recommend_help());
+                    error(result.clone());
+                    Err(result)
                 },
-            };
+            }
         },
         None => {
-            error(format!("No command received. {}", recommend_help()));
-            return;
+            let result = format!("No command received. {}", recommend_help());
+            error(result.clone());
+            Err(result)
         },
-    };
+    }
 }
 
-fn create_new_task_from_arguments(mut args: std::env::Args, to_do: &mut ToDo) {
+fn create_new_task_from_arguments(mut args: std::env::Args, to_do: &mut ToDo) -> Result<String, String> {
     match args.nth(0) {
         Some(task) => {
             let labels: Vec<String> = args.collect();
             match to_do.add(task.clone(), labels) {
                 Ok(_) => {
-                    trace(format!("New task {} created.", task));
+                    let result = format!("Task with name \"{}\" created.", task);
+                    trace(result.clone());
+                    Ok(result)
                 },
-                Err(message) => { error(message); }
-            };
+                Err(message) => {
+                    error(message.clone());
+                    Err(message)
+                },
+            }
         },
         None => {
-            error(format!("No task name received. {}", recommend_help()));
+            let result = format!("No task name received. {}", recommend_help());
+            error(result.clone());
+            Err(result)
         },
-    };
+    }
 }
 
 fn recommend_help() -> String {
     format!("Call \"project_booking help\" for aditional information.")
+}
+
+pub trait ToString
+{
+    fn to_string(&self) -> String;
+}
+
+impl ToString for Result<String, String>
+{
+    fn to_string(&self) -> String
+    {
+        match self {
+            Ok(message) => message.clone(),
+            Err(message) => message.clone(),
+        }
+    }
 }
